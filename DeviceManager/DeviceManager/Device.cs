ad83e4ed802387181e571800f2c472c6bfe57cbc
LizaChehovich <lizachehovich@gmail.com>
2017-11-17 10:46:21 +0300

    Change classes Device and Driver

--- DeviceManager/DeviceManager/Device.cs
+++ DeviceManager/DeviceManager/Device.cs
@@ -2,38 +2,12 @@
 {
     class Device
     {
-        private string _name;
-        private string _guid;
-        private string _hardwareID;
-        private string _manufacturer;
-        private Driver _driver;
-        private string _path;
-        private Status _status;
-
-        public Device(string name, string guid, string hardwareID, string manufacturer, 
-            Driver driver, string path, Status status)
-        {
-            _name = name;
-            _guid = guid;
-            _hardwareID = hardwareID;
-            _manufacturer = manufacturer;
-            _driver = driver;
-            _path = path;
-            _status = status;
-        }
-
-        public string Name => _name;
-
-        public string GUID => _guid;
-
-        public string HardwareID => _hardwareID;
-
-        public string Manufacturer => _manufacturer;
-
-        public Driver Driver => _driver;
-
-        public string Path => _path;
-
-        public Status Status => _status;
+        public string Name { get; set; }
+        public string GUID { get; set; }
+        public string HardwareID { get; set; }
+        public string Manufacturer { get; set; }
+        public Driver Driver { get; set; }
+        public string Path { get; set; }
+        public Status Status { get; set; }
     }
 }
--- DeviceManager/DeviceManager/Driver.cs
+++ DeviceManager/DeviceManager/Driver.cs
@@ -1,24 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeviceManager
+﻿namespace DeviceManager
 {
     class Driver
     {
-        private string _description;
-        private string _sysPath;
-
-        public Driver(string description, string sysPath)
-        {
-            _description = description;
-            _sysPath = sysPath;
-        }
-
-        public string Description => _description;
-
-        public string SysPath => _sysPath;
+        public string Description { get; set; }
+        public string SysPath { get; set; }
     }
 }
