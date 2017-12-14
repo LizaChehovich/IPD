499da918be4a31b11b948c8f0f7b0593a5780587
LizaChehovich <lizachehovich@gmail.com>
2017-12-14 08:54:49 +0300

    Add ConfigurationInfo class

---
+++ GlobalHooks/GlobalHooks/ConfigurationInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace GlobalHooks
+{
+    class ConfigurationInfo
+    {
+        public string Email { get; set; }
+        public long FileSize { get; set; }
+        public List<Keys> ShortcutToDisconnect { get; set; }
+        public List<Keys> ShortcutToDisplayTheWindow { get; set; }
+        public bool HiddenMode { get; set; }
+    }
+}
