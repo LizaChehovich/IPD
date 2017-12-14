1c1ed2b181332c1fa8544f4b91ca09f79bcdeade
LizaChehovich <lizachehovich@gmail.com>
2017-12-14 08:56:26 +0300

    Add function for reading and saving configuration

---
+++ GlobalHooks/GlobalHooks/ConfigurationFileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace GlobalHooks
+{
+    class ConfigurationFileController
+    {
+        private const string KeyWord = "kisa";
+
+        public ConfigurationInfo ReadFromFile()
+        {
+            try
+            {
+                var reader = new StreamReader(@"GH.conf");
+                return JsonConvert.DeserializeObject<ConfigurationInfo>(reader.ReadToEnd());
+            }
+            catch (Exception)
+            {
+                return new ConfigurationInfo();
+            }
+        }
+
+        public void SaveToFile(ConfigurationInfo configuration)
+        {
+            var writer = new StreamWriter(@"GH.conf",false);
+            writer.Write(JsonConvert.SerializeObject(configuration));
+        }
+
+    }
+}
